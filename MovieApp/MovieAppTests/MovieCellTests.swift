269dbb4c515c1a7e7e6aa83142cd8dc4062d5c5e
Alexandre Cardoso <developer.ios.alexandre@gmail.com>
2021-07-11 18:01:11 -0300

    Inclusao UI Test MovieCell

---
+++ MovieApp/MovieAppTests/MovieCellTests.swift
@@ -0,0 +1,78 @@
+//
+//  MovieCellTests.swift
+//  MovieAppTests
+//
+//  Created by Alexandre Cardoso on 11/07/21.
+//
+
+import Quick
+import Nimble
+import Nimble_Snapshots
+
+@testable import MovieApp
+
+class MovieCellTests: QuickSpec {
+	
+	override func spec() {
+		
+		describe("UI - Movie Cell") {
+						
+			it("Cell com titulo curto e sem favorito") {
+				let frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width/2.2, height: UIScreen.main.bounds.width/1.8)
+				let view = MovieCell(frame: frame)
+				let movieModel = self.createMovieModel(title: "Thor")
+				let movielCellVM = self.createMovieCellModel(movie: movieModel, favorite: false)
+				view.setupCell(viewModel: movielCellVM)
+				expect(view) == snapshot("MovieCell-Short-Title-Unfavorite")
+			}
+			
+			it("Cell com titulo curto e com favorito") {
+				let frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width/2.2, height: UIScreen.main.bounds.width/1.8)
+				let view = MovieCell(frame: frame)
+				let movieModel = self.createMovieModel(title: "Thor")
+				let movielCellVM = self.createMovieCellModel(movie: movieModel, favorite: true)
+				view.setupCell(viewModel: movielCellVM)
+				expect(view) == snapshot("MovieCell-Short-Title-Favorite")
+			}
+			
+			it("Cell com titulo longo sem favorito") {
+				let frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width/2.2, height: UIScreen.main.bounds.width/1.8)
+				let view = MovieCell(frame: frame)
+				let movieModel = self.createMovieModel(title: "Muler Maravilha 1844 - Ultimo Filme")
+				let movielCellVM = self.createMovieCellModel(movie: movieModel, favorite: false)
+				view.setupCell(viewModel: movielCellVM)
+				expect(view) == snapshot("MovieCell-Long-Title-Unfavorite")
+			}
+			
+			it("Cell com titulo longo com favorito") {
+				let frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width/2.2, height: UIScreen.main.bounds.width/1.8)
+				let view = MovieCell(frame: frame)
+				let movieModel = self.createMovieModel(title: "Muler Maravilha 1844 - Ultimo Filme")
+				let movielCellVM = self.createMovieCellModel(movie: movieModel, favorite: true)
+				view.setupCell(viewModel: movielCellVM)
+				expect(view) == snapshot("MovieCell-Long-Title-Favorite")
+			}
+			
+		}
+		
+	}
+	
+}
+
+
+extension MovieCellTests {
+	
+	func createMovieModel(title: String) -> MovieResult {
+		let movie = MovieResult(adult: false, backdrop_path: "", genre_ids: [0], id: 123,
+										original_language: "", original_title: title, overview: "",
+										popularity: 0.0, poster_path: "", release_date: "", title: "",
+										video: false, vote_average: 0.0, vote_count: 2)
+		return movie
+	}
+	
+	func createMovieCellModel(movie: MovieResult, favorite: Bool) -> MovieCellViewModel {
+		let movieCell = MovieCellViewModel(movie: movie, favorite: favorite)
+		return movieCell
+	}
+	
+}
